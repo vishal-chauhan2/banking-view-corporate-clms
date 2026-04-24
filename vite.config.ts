e6d90c1d617dfec36385e200a06131829bc72714
vishal-chauhan2 <166840154+vishal-chauhan2@users.noreply.github.com>
2026-04-24 17:06:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,3 +20,7 @@ export default defineConfig(({ mode }) => ({
     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
 }));
+
+export default defineConfig({
+  base: "/banking-view-corporate-clms/",
+});
